d73f19317aa49e42708b5e40f7f46402bfc78a1c
Samuel Medhat Farid Fahmy <1400721@eng.asu.edu.eg>
2019-04-23 02:45:18 +0200

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,75 @@
+import numpy as np
+import cv2
+
+def nothing(x):
+    pass
+cap = cv2.VideoCapture(0)
+# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+# fgbg = cv2.createBackgroundSubtractorMOG2()
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("min threshold", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("alpha", "Trackbars",100 , 300, nothing)
+cv2.createTrackbar("beta", "Trackbars",50 , 100, nothing)
+
+while(True):
+    thres = cv2.getTrackbarPos("min threshold", "Trackbars")
+    alpha = cv2.getTrackbarPos("alpha", "Trackbars")
+    beta = cv2.getTrackbarPos("beta", "Trackbars")
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    new_frame = cv2.convertScaleAbs(frame, alpha=alpha/100, beta = beta-50)
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',new_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    
+
+
+
+
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+
+    ret, thresh_img = cv2.threshold(blur,thres,255,cv2.THRESH_BINARY_INV)
+    # thresh_img = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    contours =  cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    # max_area = 0
+    # index =  0
+    # for i in len(contours):
+    #     area = cv2.contourArea(contours[i])
+    #     if area > max_area:
+    #         max_area = area
+    #         index  = i
+    if len(contours):
+        c = max(contours, key = cv2.contourArea)
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.drawContours(new_frame, [c], -1, (0,255,0), 3)
+        cv2.circle(new_frame, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(new_frame, "area: "+str(cv2.contourArea(c)) , (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        cv2.putText(new_frame, "(" + str(cX) + "," + str(cY)+")", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+     # Display the resulting frame
+    cv2.imshow('frame',new_frame)
+    cv2.imshow('thresh_img',thresh_img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    # fgmask = fgbg.apply(frame)
+    # # fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    # cv2.imshow('frame',fgmask)
+    # k = cv2.waitKey(30) & 0xff
+    # if k == 27:
+    #     break
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()

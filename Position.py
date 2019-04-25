3f64dbc61f2631ac0b06b469c37459ee4aee9355
Samuel Medhat Farid Fahmy <samuel_farid@yahoo.com>
2019-04-25 14:37:49 +0200

    updating set_position function with improvements

--- Position.py
+++ Position.py
@@ -9,16 +9,25 @@ class Position:
         self.image=None
         self.center=None
 
-    def set_position(self,image,threshold):
+    def set_position(self,image,threshold,alpha,beta):
         # get the contour of this position
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25,25))
         self.image=image
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        new_frame = cv2.convertScaleAbs(image, alpha=alpha/100, beta = beta-50)
+        gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         ret, thresh_img = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY_INV)
-        contours = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        self.contour= max(contours, key = cv2.contourArea)
-        center=cv2.moments(self.contour)
-        self.center=(int(center["m10"] / center["m00"]), int(center["m01"] / center["m00"]))
+        thresh_img2 = cv2.morphologyEx(thresh_img, cv2.MORPH_CLOSE, kernel)
+        contours = cv2.findContours(thresh_img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        if len(contours):
+            self.contour= max(contours, key = cv2.contourArea)
+            self.area = cv2.contourArea(self.contour)
+            M=cv2.moments(self.contour)
+            if (M["m00"] !=0):
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])            
+                self.center=(cX, cY)
+            
 
 
     def get_difference(self):

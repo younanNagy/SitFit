c52b8a57f729cb1bcad7d6a97d119806167e59a4
Samuel Medhat Farid Fahmy <samuel_farid@yahoo.com>
2019-05-03 19:04:51 +0200

    Update Position.py

--- Position.py
+++ Position.py
@@ -8,12 +8,14 @@ class Position:
         self.area=None
         self.image=None
         self.center=None
+        self.contour_flag = False
 
     def set_position(self,image,threshold,alpha,beta):
         # get the contour of this position
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25,25))
         self.image=image
         new_frame = cv2.convertScaleAbs(image, alpha=alpha/100, beta = beta-50)
+        self.image = new_frame
         gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         ret, thresh_img = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY_INV)
@@ -22,11 +24,17 @@ class Position:
         if len(contours):
             self.contour= max(contours, key = cv2.contourArea)
             self.area = cv2.contourArea(self.contour)
+            self.contour_flag = True
             M=cv2.moments(self.contour)
             if (M["m00"] !=0):
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])            
                 self.center=(cX, cY)
+        else:
+            self.contour_flag = False
+            self.contour= None
+            self.area = None
+            self.center = None
             
 
 
@@ -41,6 +49,6 @@ class Position:
     def Compare(self):
         pass
 
-position=Position()
-position.SetPosition(2,4)
-print(position.area,"   ",type(position.area))
+# position=Position()
+# position.SetPosition(2,4)
+# print(position.area,"   ",type(position.area))
